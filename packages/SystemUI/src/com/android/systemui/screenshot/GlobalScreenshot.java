7936bc461d989f7a985b7a93fcc4d60d7edfa6e3
Ayan Mukherjee <mukherjeeayan725@gmail.com>
2021-06-23 14:56:55 -0300

    SystemUI: Redesign screenshot menu buttons
    Change-Id: Ica2311af5dc14720c4d14e443f0a98424b3562ba

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -1054,7 +1054,6 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
 
         ScreenshotActionChip shareChip = (ScreenshotActionChip) inflater.inflate(
                 R.layout.global_screenshot_action_chip, mActionsView, false);
-        shareChip.setText(imageData.shareAction.title);
         shareChip.setIcon(imageData.shareAction.getIcon(), true);
         shareChip.setPendingIntent(imageData.shareAction.actionIntent, () -> {
             mUiEventLogger.log(ScreenshotEvent.SCREENSHOT_SHARE_TAPPED);
@@ -1066,7 +1065,6 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
 
         ScreenshotActionChip editChip = (ScreenshotActionChip) inflater.inflate(
                 R.layout.global_screenshot_action_chip, mActionsView, false);
-        editChip.setText(imageData.editAction.title);
         editChip.setIcon(imageData.editAction.getIcon(), true);
         editChip.setPendingIntent(imageData.editAction.actionIntent, () -> {
             mUiEventLogger.log(ScreenshotEvent.SCREENSHOT_EDIT_TAPPED);
@@ -1078,7 +1076,6 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
 
         ScreenshotActionChip deleteChip = (ScreenshotActionChip) inflater.inflate(
                 R.layout.global_screenshot_action_chip, mActionsView, false);
-        deleteChip.setText(imageData.deleteAction.title);
         deleteChip.setIcon(imageData.deleteAction.getIcon(), true);
         deleteChip.setPendingIntent(imageData.deleteAction.actionIntent, () -> {
             mUiEventLogger.log(ScreenshotEvent.SCREENSHOT_DELETE_TAPPED);

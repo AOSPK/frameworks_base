aca4fcd9556f0e0c38cf66a4913c551718da27dd
programminghoch10 <hoch10@kabelbw.de>
2021-07-12 17:55:28 +0200

    SystemUI: VolumeDialogImpl: Fix volume panel permanently visible
    * The Volume Panel is permanently visible when changing the panel side while the panel is open until SystemUI is restarted
    * This change removes the Panel View when changing the panel side
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/3482
    
    Change-Id: Ic3ca5a196315e96024121c995cc6282fb74f6e4c

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -422,8 +422,7 @@ public class VolumeDialogImpl implements VolumeDialog,
                 if (mVolumePanelOnLeft != volumePanelOnLeft) {
                     mVolumePanelOnLeft = volumePanelOnLeft;
                     mHandler.post(() -> {
-                        // Trigger panel rebuild on next show
-                        mConfigChanged = true;
+                        mControllerCallbackH.onConfigurationChanged();
                     });
                 }
             }
